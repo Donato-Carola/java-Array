a0c42f1ed3832532e83d5fbc9b2f3de74b2836b5
Donato Carola <don94@live.it>
2024-05-25 17:17:15 +0200

    sum element array

--- main.java
+++ main.java
@@ -34,23 +34,39 @@
 
 //Dato un array di interi già valorizzato, stampare tutti gli elementi maggiori di 10
 
+// import java.util.Scanner;
+
+// public class main {
+
+//     public static void main(String[] args) {
+//          int [] array = {55, 1, 10, 80, 75, 60, 45};
+
+//          for(int i = 0; i < array.length; i++){
+//             if( array[i] > 10){
+//               System.out.println(array[i]);  
+//             }
+
+//          }
+
+//     }
+// }
+
+//!esercizio 4
+
+//Dato un array di interi già valorizzato, stampare la somma di tutti gli elementi
+
 import java.util.Scanner;
 
 public class main {
 
-    
     public static void main(String[] args) {
-         int [] array = {55, 1, 10, 80, 75, 60, 45};
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
 
-         for(int i = 0; i < array.length; i++){
-            if( array[i] > 10){
-              System.out.println(array[i]);  
-            }
-              
-         }
-
-        
-
+        }
+        System.out.println(sum);
 
     }
 }

6c82eb36b5e8362253d4c6c4fc467f88b1581802
Donato Carola <don94@live.it>
2024-05-25 17:13:10 +0200

    exercise array java greater of 10

--- main.java
+++ main.java
@@ -16,16 +16,41 @@
 
 //Dato un array di interi già valorizzato, stampare il contenuto
 
+// import java.util.Scanner;
+
+// public class main {
+
+//     public static void main(String[] args) {
+//         int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//         for (int i = 0; i < array.length; i++) {
+//             System.out.print(" " + array[i]);
+//         }
+
+//     }
+// }
+
+//! esercizio 3
+
+//Dato un array di interi già valorizzato, stampare tutti gli elementi maggiori di 10
+
 import java.util.Scanner;
 
 public class main {
 
+    
     public static void main(String[] args) {
-        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+         int [] array = {55, 1, 10, 80, 75, 60, 45};
 
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(" " + array[i]);
-        }
+         for(int i = 0; i < array.length; i++){
+            if( array[i] > 10){
+              System.out.println(array[i]);  
+            }
+              
+         }
+
+        
+
 
     }
 }
